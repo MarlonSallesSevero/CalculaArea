3046bbf539f8570aaf0fd2ff7fc1cbb680703e40
Marlon Salles <72519815+MarlonSallesSevero@users.noreply.github.com>
2021-12-07 23:26:20 -0300

    Add files via upload

---
+++ retang.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace POO
+{
+    public class retang
+    {
+        public float Alt;
+        public float Larg;
+
+        public float Area()
+        {
+            float area = Alt * Larg;
+            return area;
+        }
+
+        public float Per()
+        {
+            float per = (2 * Alt) + (2 * Larg);
+            return per;
+        }
+        public double Diag()
+        {
+            double diag = Math.Sqrt(Math.Pow(Alt, 2) + Math.Pow(Larg, 2));
+            return diag;
+        }
+
+     
+    }
+}
